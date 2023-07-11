008045493975eeb7f973c4395fef5aab11cf87e9
devMart <umbaomart@gmail.com>
2023-07-11 13:11:37 +0800

    [Reusability] Challenge - Button w/ Variants

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,31 @@ function App() {
   return (
     <main>
       <Button
+        size="sm"
+        onClick={() => console.log("Logging in...")}
+      >
+        Log in with Google
+      </Button>
+      <Button
         size="lg"
         className='green'
+        variant="success"
+        onClick={() => console.log("Logging in...")}
+      >
+        Log in with Google
+      </Button>
+      <Button
+        size="sm"
+        className='green'
+        variant="warning"
+        onClick={() => console.log("Logging in...")}
+      >
+        Log in with Google
+      </Button>
+      <Button
+        size="sm"
+        className='green'
+        variant="danger"
         onClick={() => console.log("Logging in...")}
       >
         Log in with Google
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,11 @@
 import classNames from "classnames";
 
-function Button({ children, size, className, ...props }) {
+function Button({ children, className, size, variant, ...props }) {
 
     let sizeClass = size ? `button-${size}` : '';
+    let variantClass = variant ? `button-${variant}` : '';
 
-    const allClasses = classNames(sizeClass, className);
+    const allClasses = classNames(sizeClass, className, variantClass);
 
     return (
         <button className={allClasses} {...props}>{children}</button>
