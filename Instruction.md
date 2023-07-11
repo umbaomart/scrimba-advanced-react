08c92a7807d6bf7955e3c00a58aa646c5be2d49f
devMart <umbaomart@gmail.com>
2023-07-11 13:54:20 +0800

    [Reusability] Mega Challenge - Overload Avatar Component

---
+++ Instruction.md
@@ -0,0 +1,23 @@
+/**
+ * Challenge: Create a flexible Avatar component!
+ *
+ * Check the slides for notes on how the component
+ * should be used.
+ *
+ * Each of the 3 different avatars should have a
+ * wrapper div with the classes below:
+ *
+ * With image: `avatar`
+ * With initials: `avatar avatar-letters`
+ * Anonymous: `avatar avatar-icon`
+ *
+ * E.g. <Avatar>BZ</Avatar> should render
+ * <div className="avatar avatar-letters">...</div>
+ *
+ * Check the hints.md file if you are really stuck.
+ *
+ * EXTRA CREDIT:
+ * Randomize the background color of the non-image
+ * avatars. Check the styles.css for some pre-written
+ * color classes to add to the wrapper div.
+ */
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import Button from './components/Button'
+import Avatar from './components/Avatar'
+import bob from './assets/bob.jpg'
 
 import './App.css'
 
@@ -6,36 +7,11 @@ function App() {
 
   return (
     <main>
-      <Button
-        size="sm"
-        onClick={() => console.log("Logging in...")}
-      >
-        Log in with Google
-      </Button>
-      <Button
-        size="lg"
-        className='green'
-        variant="success"
-        onClick={() => console.log("Logging in...")}
-      >
-        Log in with Google
-      </Button>
-      <Button
-        size="sm"
-        className='green'
-        variant="warning"
-        onClick={() => console.log("Logging in...")}
-      >
-        Log in with Google
-      </Button>
-      <Button
-        size="sm"
-        className='green'
-        variant="danger"
-        onClick={() => console.log("Logging in...")}
-      >
-        Log in with Google
-      </Button>
+      <Avatar src={bob} alt="bob" />
+      <br />
+      <Avatar>BZ</Avatar>
+      <br />
+      <Avatar />
     </main>
   )
 }
