23105f63f68065ab2cbf5e0cb15ab2e1d653e8ba
devMart <umbaomart@gmail.com>
2023-07-18 06:12:21 +0800

    [Reusability] Intro to Refs

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
-)
+export default function Main() {
+    const [on, setOn] = React.useState(false)
+    const renderCount = React.useRef(0)
+
+    console.log(renderCount);
+
+    function forceRender() {
+        setOn((prevOn) => !prevOn)
+    }
+
+    function incrementRenderCount() {
+        renderCount.current++
+    }
+
+    React.useEffect(() => {
+        renderCount.current++
+    })
+
+    return (
+        <>
+            <h3>Understanding refs</h3>
+            <button onClick={forceRender}>Force re-render w/ state change</button>
+            <button onClick={incrementRenderCount}>Increment Ref Count</button>
+            <h4>Render count: {renderCount.current} </h4>
+        </>
+    )
+}
