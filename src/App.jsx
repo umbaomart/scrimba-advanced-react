fd73f3f5b523b1e4a60bf1f34634ac02f6556c3f
devMart <umbaomart@gmail.com>
2023-07-22 04:24:25 +0800

    [React Router] Initial Setup and BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,12 @@
-// import Star from "./Star";
-import Menu from "./components/Menu/index";
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-function App() {
-  return (
-    <>
-      <Menu onOpen={() => console.log("Opened/Closed")}>
-        <Menu.Button>Menu</Menu.Button>
-        <Menu.Dropdown>
-          <Menu.Item>Home</Menu.Item>
-          <Menu.Item>About</Menu.Item>
-          <Menu.Item>Contact</Menu.Item>
-          <Menu.Item>Blog</Menu.Item>
-        </Menu.Dropdown>
-      </Menu>
-    </>
-  )
+export default function App() {
+    return (
+        <BrowserRouter>
+            <div>
+                <h1>React App</h1>
+            </div>
+        </BrowserRouter>
+    );
 }
-
-{/* <Star /> */}
-{/* <Toggle onToggle={() => console.log('Toggled')}>
-        <Toggle.Button>
-          <Toggle.Display>
-            {(on) => {
-              return (
-                <div className={`box ${on ? 'filled' : ''}`}></div>
-              )
-            }}
-          </Toggle.Display>
-        </Toggle.Button>
-      </Toggle> */}
-
-// <br />
-export default App;
--- src/components/Menu/MenuItem.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function MenuItem({ children }) {
-    return (
-        <div className="menu-item">
-            {children}
-        </div>
-    )
-}
