da56f2d12adc5e35d7e5f59e5018d183ca10ed50
devMart <umbaomart@gmail.com>
2023-08-01 03:53:42 +0800

    [React Router] Building out the Host Van Detail page

--- src/pages/Host/HostVansDetails.jsx
+++ src/pages/Host/HostVansDetails.jsx
@@ -1,5 +1,26 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
 export default function HostVansDetails() {
-    return (
-        <h1>Vans List Item</h1>
-    )
+    const [van, setVan] = React.useState(null);
+    const {id} = useParams();
+
+    React.useEffect(() => {
+        fetch(`/api/host/vans/${id}`)
+            .then(res => res.json())
+            .then(data => setVan(data.vans))
+    }, [id])
+
+    return van ? (
+        <div className="cardItem">
+            <div className="cardItem__image">
+                <img src={van.imageUrl} alt={van.name} />
+            </div>
+            <div className="cardItem__info">
+                <p>{van.type}</p>
+                <h2>{van.name}</h2>
+                <p>{van.price}</p>
+            </div>
+        </div>
+    ) : <h2>Loading...</h2>
 }
