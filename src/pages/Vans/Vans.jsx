3e54eb39e4259cda7e79fd157320472f03b739a3
dev_mart <umbaomart@gmail.com>
2023-09-14 05:54:30 +0800

    Adding search params filter

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,14 +1,21 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import { useSearchParams } from "react-router-dom"
 
 export default function Vans() {
+    const [searchParams, setSearchParams] = useSearchParams()
+    const typeFilter = searchParams.get("type")
+
     const [vans, setVans] = React.useState([])
 
     React.useEffect(() => {
         fetch("/api/vans")
             .then(res => res.json())
-            .then(data => setVans(data.vans))
-    }, [])
+            .then(data => {
+                const vansList = data.vans;
+                typeFilter ? setVans(vansList.filter(van => van.type === typeFilter)) : setVans(data.vans)
+            })
+    }, [typeFilter])
 
     const vanElements = vans.map(van => {
         return (
@@ -25,9 +32,21 @@ export default function Vans() {
         )
     })
 
+
     return (
+
         <div className="van-list-container">
             <h1>Explore our van options</h1>
+            <div className="van-list-filter-buttons">
+                <button className="van-type simple" onClick={() => setSearchParams({type: "simple"})}>Simple</button>
+                <button className="van-type luxury" onClick={() => setSearchParams({type: "luxury"})}>Luxury</button>
+                <button className="van-type rugged" onClick={() => setSearchParams({type: "rugged"})}>Rugged</button>
+                <button className="van-type clear-filters" onClick={() => setSearchParams({})}>Clear</button>
+                {/* <Link className="van-type simple" to="?type=simple">Simple</Link>
+                <Link className="van-type luxury" to="?type=luxury">Luxury</Link>
+                <Link className="van-type rugged" to="?type=rugged">Rugged</Link>
+                <Link className="van-type clear-filters" to=".">Clear</Link> */}
+            </div>
             <div className="van-list">
                 {vanElements}
             </div>
